8e8e5af1a8274226c9cdc0806c734cbaadaf6093
Dominguez Gaston Matias <gastonmd14@gmail.com>
2022-03-18 16:12:22 -0300

    Modify seeder to populate table Users

--- config/database/seeders/20220318185921-User.js
+++ config/database/seeders/20220318185921-User.js
@@ -2,23 +2,17 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Users', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      userName: 'John.Doe',
+      password: '123456',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Users', null, {});
   }
 };
