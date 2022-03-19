898b723558437c8542af823025f17b2a83613893
Dominguez Gaston Matias <gastonmd14@gmail.com>
2022-03-19 10:58:27 -0300

    modify structure to 3 layers responsabilities

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authController = require('./src/controllers/auth');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authController);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
